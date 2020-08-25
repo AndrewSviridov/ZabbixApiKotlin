f13cd1cb1aca52e130d1907795e96ff8883f0717
asviridov <a.sviridov@nachislim.ru>
2020-08-25 16:51:02 +0700

    commit api 4.5. change List type in RequestEvent.kt to ArrayList

--- src/main/kotlin/Event/RequestEvent.kt
+++ src/main/kotlin/Event/RequestEvent.kt
@@ -10,16 +10,16 @@ class RequestEvent : ZabbixApiRequest() {
     val params = Params()
 
     class Params : GetRequestCommonParams() {
-        var eventids: List<String>? = null
-        var groupids: List<String>? = null
-        var hostids: List<String?>? = null
-        var objectids: List<String?>? = null
-        var applicationids: List<String?>? = null
+        var eventids: ArrayList<String>? = null
+        var groupids: ArrayList<String>? = null
+        var hostids: ArrayList<String?>? = null
+        var objectids: ArrayList<String?>? = null
+        var applicationids: ArrayList<String?>? = null
         var source: Int? = null
         var `object`: Int? = null
         var acknowledged: Boolean? = null
         var suppressed: Boolean? = null
-        var severities: List<Int?>? = null
+        var severities: ArrayList<Int?>? = null
         var evaltype: Int? = null
         var tags: ArrayList<Tag>? = null
         var eventid_from: String? = null
@@ -28,13 +28,13 @@ class RequestEvent : ZabbixApiRequest() {
         var time_till: Long? = null
         var problem_time_from: Long? = null
         var problem_time_till: Long? = null
-        var value: List<Int?>? = null
-        var selectHosts: List<String?>? = null
-        var selectRelatedObject: List<String?>? = null
-        var select_alerts: List<String?>? = null
-        var select_acknowledges: List<String?>? = null
-        var selectTags: List<String?>? = null
-        var selectSuppressionData: List<String?>? = null
+        var value: ArrayList<Int?>? = null
+        var selectHosts: ArrayList<String?>? = null
+        var selectRelatedObject: ArrayList<String?>? = null
+        var select_alerts: ArrayList<String?>? = null
+        var select_acknowledges: ArrayList<String?>? = null
+        var selectTags: ArrayList<String?>? = null
+        var selectSuppressionData: ArrayList<String?>? = null
 
 
     }
